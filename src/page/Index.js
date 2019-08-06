d8c531dbcaa4cc1c05b841e28dfc578eb0fee242
Beginner <l_strive@163.com>
2019-08-06 17:32:54 +0800

    数据过滤筛选--FilterTable

--- src/page/Index.js
+++ src/page/Index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import TempCalculator from '../component/TempCalculator';
 import NameForm from '../component/NameForm';
+import FilterableProductTable from '../component/FilterableProductTable';
 
 class Index extends React.Component{
 	render(){
 		return(
 			<div className="indexceshi">
 				<TempCalculator></TempCalculator>
+				<div style={{ height : '10px' , background : '#ccc' }}></div>
 				<NameForm></NameForm>
+				<div style={{ height : '10px' , background : '#ccc' }}></div>
+				<FilterableProductTable></FilterableProductTable>
 			</div>
 		)
 	}
